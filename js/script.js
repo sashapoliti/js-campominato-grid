9b70f43d4ea514a85df1f9c6b4a79fc6410987ce
sashapoliti <sas.politi@gmail.com>
2024-03-15 14:15:30 +0100

    create function createBoxElement for add boxes

--- js/script.js
+++ js/script.js
@@ -3,3 +3,7 @@ Ogni cella ha un numero progressivo, da 1 a 100.
 Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata. */
 
+const playButton = document.querySelector('header button');
+
+console.log(createBoxElement());
+
--- js/utility.js
+++ js/utility.js
@@ -1,3 +1,9 @@
+function createBoxElement() {
+  const box = document.createElement('div');
+  box.classList.add('box');
+  return box;
+}
+
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
   }
