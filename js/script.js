6ad276e8a0d9fb8744c37d2533ba863ce872fd9b
sashapoliti <sas.politi@gmail.com>
2024-03-15 15:43:29 +0100

    bonus: add select mode with values and if inside button event

--- js/script.js
+++ js/script.js
@@ -5,21 +5,52 @@ Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed 
 
 const playButton = document.querySelector('header button'); /* take button */
 const fieldElement = document.getElementById('field');
+const modeElement = document.getElementById('mode');
 
 //console.log(createBox());
 
 playButton.addEventListener('click', function() {
     fieldElement.innerHTML = ''; //reset the field
-    for (let i = 0; i < 100; i++) {
-        const boxElement = createBox(i + 1);
-        //console.log(boxElement);
-        fieldElement.appendChild(boxElement);
 
-        //box clickable
-        boxElement.addEventListener('click', function() {
-            boxElement.classList.add('active');
-            console.log(i + 1);
-        })
+    const mode = modeElement.value;
+    if (mode === 'medium') {
+        for (let i = 0; i < 81; i++) {
+            const boxElement = createBox(i + 1);
+            boxElement.classList.add('medium')
+            //console.log(boxElement);
+            fieldElement.appendChild(boxElement);
+    
+            //box clickable
+            boxElement.addEventListener('click', function() {
+                boxElement.classList.add('active');
+                console.log(i + 1);
+            })
+        }  
+    } else if (mode === 'hard') {
+        for (let i = 0; i < 49; i++) {
+            const boxElement = createBox(i + 1);
+            boxElement.classList.add('hard')
+            //console.log(boxElement);
+            fieldElement.appendChild(boxElement);
+    
+            //box clickable
+            boxElement.addEventListener('click', function() {
+                boxElement.classList.add('active');
+                console.log(i + 1);
+            })
+        }  
+    } else {
+        for (let i = 0; i < 100; i++) {
+            const boxElement = createBox(i + 1);
+            //console.log(boxElement);
+            fieldElement.appendChild(boxElement);
+    
+            //box clickable
+            boxElement.addEventListener('click', function() {
+                boxElement.classList.add('active');
+                console.log(i + 1);
+            })
+        }
     }
 })
 
